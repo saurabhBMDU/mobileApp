0c40bba4673541a962f15c9ec04af4f664d0a183
vikram singh <vikramsinghkhinchi5@gmail.com>
2024-04-03 18:51:37 +0530

    added smart prescriptn

--- src/Components/smartPresscription.tsx
+++ src/Components/smartPresscription.tsx
@@ -8,8 +8,19 @@ import {
   ScrollView,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import Headerr from '../ReuseableComp/Headerr';
 
 interface Medicine {
+  id: number;
+  name: string;
+}
+
+interface Medicine2 {
+  id: number;
+  name: string;
+}
+
+interface Medicine3 {
   id: number;
   name: string;
 }
@@ -21,6 +32,8 @@ interface Test {
 
 const SmartPrescriptionPage: React.FC = () => {
   const [medicines, setMedicines] = useState<Medicine[]>([]);
+  const [medicines2, setMedicines2] = useState<Medicine2[]>([]);
+  const [medicines3, setMedicines3] = useState<Medicine3[]>([]);
   const [tests, setTests] = useState<Test[]>([]);
 
   const addMedicine = (name: string) => {
@@ -32,8 +45,39 @@ const SmartPrescriptionPage: React.FC = () => {
   };
 
   const deleteMedicine = (id: number) => {
-    console.log('id',id)
+    console.log('id', id);
     setMedicines(medicines.filter(medicine => medicine.id !== id));
+  };
+
+
+  
+  const addMedicine2 = (name: string) => {
+    const newMedicine: Medicine2 = {
+      id: Date.now(),
+      name: name,
+    };
+    setMedicines2([...medicines, newMedicine]);
+  };
+
+  const deleteMedicine2 = (id: number) => {
+    console.log('id', id);
+    setMedicines2(medicines.filter(medicine => medicine.id !== id));
+  };
+
+
+  
+  
+  const addMedicine3 = (name: string) => {
+    const newMedicine: Medicine3 = {
+      id: Date.now(),
+      name: name,
+    };
+    setMedicines3([...medicines, newMedicine]);
+  };
+
+  const deleteMedicine3 = (id: number) => {
+    console.log('id', id);
+    setMedicines3(medicines.filter(medicine => medicine.id !== id));
   };
 
   const addTest = (name: string) => {
@@ -50,120 +94,179 @@ const SmartPrescriptionPage: React.FC = () => {
 
   return (
     <View style={styles.container}>
+        {/* <Headerr user={true} height={true} /> */}
+        <Headerr secndheader={true} label="SMART PRESCRIPTION" />
       <ScrollView>
         <Text style={styles.sectionHeader}>Frequently Used Medicines</Text>
 
-       <View
-       style={{
-        padding:5,
-        margin:5,
-        borderRadius:10,
-        elevation: 5,
-        backgroundColor:'#fff'
-       }}
-       >
-        <View
-          style={{
-            backgroundColor: '#fff',
-            paddingLeft:10,
-            paddingRight:10,
-            paddingTop:10,
-          }}>
-          <FlatList
-            data={medicines}
-            renderItem={({item}) => (
-              <View style={styles.itemContainer}>
-                <View
-                  style={{
-                    width: '85%',
-                  }}>
-                  <Text
-                    style={{
-                      color: '#194E5C',
-                      fontWeight: 'bold',
-                    }}>
-                    {item.name}
-                  </Text>
-                </View>
+         {/* Frequently Used Medicine */}
+        <View style={styles.flatViewMainParent}>
+          <View style={styles.flatViewSubParent}>
+            <FlatList
+              data={medicines}
+              renderItem={({item}) => (
+                <View style={styles.itemContainer}>
+                  <View style={styles.itemNameParent}>
+                    <Text style={styles.itemName}>{item.name}</Text>
+                  </View>
 
-                <View
-                  style={{
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                  }}>
-                  <TouchableOpacity
-                    style={{
-                      marginRight: 5,
-                      borderWidth: 2,
-                      borderColor: '#4ebfb2',
-                      borderRadius: 10,
-                    }}
-                    onPress={() => deleteMedicine(item.id)}>
-                    <Icon name="x" style={styles.icon} />
-                  </TouchableOpacity>
-                  <TouchableOpacity
-                  // onPress={() => }
-                  >
-                    <Icon name="edit" style={styles.icon} />
-                  </TouchableOpacity>
+                  <View style={styles.ButtonParent}>
+                    <TouchableOpacity
+                      style={styles.DeleteButton}
+                      onPress={() => deleteMedicine(item.id)}>
+                      <Icon name="x" style={styles.icon} />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                    // onPress={() => }
+                    >
+                      <Icon name="edit" style={styles.icon} />
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              )}
+              keyExtractor={item => item.id.toString()}
+            />
+          </View>
+
+          <TouchableOpacity
+            style={styles.container1}
+            onPress={() =>
+              addMedicine(
+                'New Medicine this is awesome medicne i nlakdnlkfdslakf jlka klja   lad fljalf',
+              )
+            }>
+            <View style={styles.row}>
+              <View style={styles.plusIconContainer}>
+                <View style={styles.plusIconBackground}>
+                  <Icon name="plus" size={24} color="white" />
                 </View>
               </View>
-            )}
-            keyExtractor={item => item.id.toString()}
-          />
+              <TouchableOpacity
+                onPress={() =>
+                  addMedicine(
+                    'New Medicine this is awesome medicne i nlakdnlkfdslakf jlka klja   lad fljalf',
+                  )
+                }
+                style={styles.addButton1}>
+                <Text style={styles.addButtonText1}>Add Medicine</Text>
+              </TouchableOpacity>
+            </View>
+          </TouchableOpacity>
         </View>
 
+        {/* Frequently used test */}
+        <Text style={styles.sectionHeader}>Frequently Used Tests</Text>
+        <View style={styles.flatViewMainParent}>
+          <View style={styles.flatViewSubParent}>
+            <FlatList
+              data={medicines2}
+              renderItem={({item}) => (
+                <View style={styles.itemContainer}>
+                  <View style={styles.itemNameParent}>
+                    <Text style={styles.itemName}>{item.name}</Text>
+                  </View>
 
-  {/* <View
-   style={{
-    flexDirection:'row',
-    backgroundColor:'red',
-    // alignSelf:'center',
-    // justifyContent:'center',
-   }}
-  >
-
-<View style={styles.plusIconContainer}>
-      <View style={styles.plusIconBackground}>
-        <Icon name="plus" size={24} color="white" />
-      </View>
-    </View>
-
-        <View>
-        <TouchableOpacity
-          style={styles.addButton}
-          onPress={() =>
-            addMedicine(
-              'New Medicine this is awesome medicne i nlakdnlkfdslakf jlka klja   lad fljalf',
-            )
-          }>
-          <Text style={styles.addButtonText}>Add Medicine</Text>
-        </TouchableOpacity>
-        </View>
-        </View> */}
-
-
-<TouchableOpacity style={styles.container1}>
-      <View style={styles.row}>
-        <View style={styles.plusIconContainer}>
-          <View style={styles.plusIconBackground}>
-            <Icon name="plus" size={24} color="white" />
+                  <View style={styles.ButtonParent}>
+                    <TouchableOpacity
+                      style={styles.DeleteButton}
+                      onPress={() => deleteMedicine2(item.id)}>
+                      <Icon name="x" style={styles.icon} />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                    // onPress={() => }
+                    >
+                      <Icon name="edit" style={styles.icon} />
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              )}
+              keyExtractor={item => item.id.toString()}
+            />
           </View>
-        </View>
-        <TouchableOpacity
-          style={styles.addButton1}
-          onPress={() =>
-            addMedicine(
-              'New Medicine this is awesome medicne i nlakdnlkfdslakf jlka klja   lad fljalf',
-            )
-          }>
-          <Text style={styles.addButtonText1}>Add Medicine</Text>
-        </TouchableOpacity>
-      </View>
-    </TouchableOpacity>
 
-
+          <TouchableOpacity
+            style={styles.container1}
+            onPress={() =>
+              addMedicine2(
+                'New Medicine this is awesome medicne i nlakdnlkfdslakf jlka klja   lad fljalf',
+              )
+            }>
+            <View style={styles.row}>
+              <View style={styles.plusIconContainer}>
+                <View style={styles.plusIconBackground}>
+                  <Icon name="plus" size={24} color="white" />
+                </View>
+              </View>
+              <TouchableOpacity
+                onPress={() =>
+                  addMedicine2(
+                    'New Medicine this is awesome medicne i nlakdnlkfdslakf jlka klja   lad fljalf',
+                  )
+                }
+                style={styles.addButton1}>
+                <Text style={styles.addButtonText1}>Add Medicine</Text>
+              </TouchableOpacity>
+            </View>
+          </TouchableOpacity>
         </View>
+
+        {/* Frequently Used Instructions */}
+           {/* Frequently used test */}
+           <Text style={styles.sectionHeader}>Frequently Used Instructions</Text>
+        <View style={styles.flatViewMainParent}>
+          <View style={styles.flatViewSubParent}>
+            <FlatList
+              data={medicines3}
+              renderItem={({item}) => (
+                <View style={styles.itemContainer}>
+                  <View style={styles.itemNameParent}>
+                    <Text style={styles.itemName}>{item.name}</Text>
+                  </View>
+
+                  <View style={styles.ButtonParent}>
+                    <TouchableOpacity
+                      style={styles.DeleteButton}
+                      onPress={() => deleteMedicine3(item.id)}>
+                      <Icon name="x" style={styles.icon} />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                    // onPress={() => }
+                    >
+                      <Icon name="edit" style={styles.icon} />
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              )}
+              keyExtractor={item => item.id.toString()}
+            />
+          </View>
+
+          <TouchableOpacity
+            style={styles.container1}
+            onPress={() =>
+              addMedicine3(
+                'New Medicine this is awesome medicne i nlakdnlkfdslakf jlka klja   lad fljalf',
+              )
+            }>
+            <View style={styles.row}>
+              <View style={styles.plusIconContainer}>
+                <View style={styles.plusIconBackground}>
+                  <Icon name="plus" size={24} color="white" />
+                </View>
+              </View>
+              <TouchableOpacity
+                onPress={() =>
+                  addMedicine3(
+                    'New Medicine this is awesome medicne i nlakdnlkfdslakf jlka klja   lad fljalf',
+                  )
+                }
+                style={styles.addButton1}>
+                <Text style={styles.addButtonText1}>Add Medicine</Text>
+              </TouchableOpacity>
+            </View>
+          </TouchableOpacity>
+        </View>
+
       </ScrollView>
     </View>
   );
@@ -199,21 +302,21 @@ const styles = StyleSheet.create({
     // backgroundColor: '#fff',
     padding: 10,
     borderRadius: 5,
-    margin:10,
-    alignItems:'flex-start',
+    margin: 10,
+    alignItems: 'flex-start',
     justifyContent: 'center',
     // marginTop: 20,
     // margin:10,
-    marginBottom:20,
+    marginBottom: 20,
   },
   addButtonText: {
     color: 'orange',
     fontWeight: 'bold',
-    fontSize:20,
+    fontSize: 20,
   },
   plusIconContainer: {
-    width: 35,
-    height: 35,
+    width: 30,
+    height: 30,
     borderRadius: 40,
     backgroundColor: 'orange',
     justifyContent: 'center',
@@ -222,30 +325,16 @@ const styles = StyleSheet.create({
   plusIconBackground: {
     borderRadius: 40,
     backgroundColor: 'orange',
-    padding: 5,
+    // padding: 5,
   },
   container1: {
     // backgroundColor: 'red',
+    height:50
   },
   row: {
     flexDirection: 'row',
     alignItems: 'center',
-    padding: 10,
   },
-//   plusIconContainer: {
-//     width: 35,
-//     height: 35,
-//     borderRadius: 40,
-//     backgroundColor: 'orange',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     marginRight: 10,
-//   },
-//   plusIconBackground: {
-//     borderRadius: 40,
-//     backgroundColor: 'orange',
-//     padding: 5,
-//   },
   addButton1: {
     // backgroundColor: '#ffa500',
     padding: 10,
@@ -254,9 +343,42 @@ const styles = StyleSheet.create({
   addButtonText1: {
     color: '#ffa500',
     // fontWeight: 'bold',
-    fontSize:20,
+    fontSize: 20,
   },
-
+  itemName: {
+    color: '#194E5C',
+    fontWeight: 'bold',
+    borderBottomWidth: 0.5, // Add bottom border
+    borderBottomColor: 'gray', // Border color
+    paddingBottom: 5,
+    fontSize: 17,
+  },
+  flatViewMainParent: {
+    padding: 10,
+    margin: 10,
+    borderRadius: 10,
+    elevation: 5,
+    backgroundColor: '#fff',
+  },
+  flatViewSubParent: {
+    backgroundColor: '#fff',
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 10,
+  },
+  itemNameParent: {
+    width: '85%',
+  },
+  ButtonParent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  DeleteButton: {
+    marginRight: 5,
+    borderWidth: 2,
+    borderColor: '#4ebfb2',
+    borderRadius: 10,
+  },
 });
 
 export default SmartPrescriptionPage;
